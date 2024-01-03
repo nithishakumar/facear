79a75c10da02697994c69b68a9c98f13cb1544b6
gaohaihan <hhgao@umich.edu>
2024-01-02 21:23:40 -0500

    DevBlog Page Updated

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,15 +10,20 @@ function Post({ post }) {
                     <h1>{post.title}</h1>
                     <p>{post.date}</p>
                     <p>{post.greeting}</p>
-                    <div className='post-video'>
-                        <iframe title="Youtube Player" src={`https://www.youtube.com/embed/${post.youtubeEmbedCode}`}/>
-                    </div>
+                    {post.youtubeEmbedCode === "" ? (<></>) : (
+                                <div className='post-video'>
+                                    <iframe title="Youtube Player" src={`https://www.youtube.com/embed/${post.youtubeEmbedCode}`}/>
+                                </div>
+                            )}
                     <ul className='post-topics'>
                         {post.topics.map((topic) =>
                             <li key={topic.id} className='post-topic'>
                                 <h1>{topic.title}</h1>
                                 {topic.img === "" ? (<></>) : (
                                     <img src={process.env.PUBLIC_URL + `/img/${topic.img}`}></img>
+                                )}
+                                {topic.youtubeEmbedCode === "" ? (<></>) : (
+                                    <iframe title="Youtube Player" src={`https://www.youtube.com/embed/${topic.youtubeEmbedCode}`}/>
                                 )}
                                 <p>{topic.text.map((text) =>
                                     <p>{text}</p>
