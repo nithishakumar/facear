c16f975a8ed324cd1c544e67a7a54d0d03133ee7
nithishakumar <nithisha@NITHISHA>
2024-05-30 13:53:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,41 @@ import Home from './Home.js'
 import Development from './Demo.js';
 import Footer from './Footer.js';
 import About from './About.js';
+import {remoteApiServicesFactory } from '@snap/camera-kit'
 
 function App() {
+
+  /*
+  const facearendpoint1 = {
+      apiSpecId: "b032a38e-4fb9-4719-a0b5-b31d1ce63900",
+      getRequestHandler: (request, lens) => {
+           // Check if the endpointId matches the expected endpoint
+           if (request.endpointId === "facearendpoint1") {
+              // Your custom logic to handle the request for the expected endpoint
+              // This function must return a RemoteApiRequestHandler or undefined
+              // Example logic:
+              // Process the request and create a response
+              const response = {
+                status: 'success',
+                metadata: { 'content-type': 'application/json' },
+                body: new TextEncoder().encode(JSON.stringify({ message: "secret unlocked" }))
+              };
+            // Return a function that will handle the response
+            return (reply) => {
+              reply(response);
+            };
+      } else {
+          // If the request is for a different endpoint, return undefined to indicate that it should not be handled
+          console.log("bad request");
+          return undefined;
+      }
+    }
+  };
+
+  const remoteApiServices = remoteApiServicesFactory();
+  remoteApiServices.push(facearendpoint1);
+  */
+
   return (
     <div className="App">
       <Router>
--- src/Demo.js
+++ src/Demo.js
@@ -7,7 +7,6 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/
 
 
 async function renderAR() {
-  
   // Bootstrap the CameraKit Web SDK: Download WebAssembly runtime and configure SDK
   const apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE1NjQwNjk0LCJzdWIiOiI5YmFmZmI5Ni01ZDNlLTQ2MzUtYmIwNC00ZGFhYzNiZmQ0YTh-U1RBR0lOR345ZjJkN2Q5Ni00OTdkLTQ1YjYtODcxYy1mZDg4MzcxYjRmNTYifQ.k-Rl8uGTuACnIfjZrjMi4OSC26OStxBF6wjipB_hDYI";
   const cameraKit = await bootstrapCameraKit({ apiToken });
@@ -28,10 +27,12 @@ async function renderAR() {
   // Loading a single lens and apply it to the session
   var lensID = "ed7e4ee8-bc4f-4860-8cbc-ea290651e867";
   var lensGroupID = "1002ed8b-a97a-42f0-842f-21b57f4a8a42";
+
+  var tempLensId = "0e0463a1-11bc-403b-96ea-27f9dac90730";
   const lens = await cameraKit.lensRepository.loadLens(lensID, lensGroupID);
   await session.applyLens(
     lens,
-    { launchParams: { text: "Some Text that we will use with a Lens" }}
+    { launchParams: { text: "Some Text that we will use with a Lens" } }
 );
 
   await session.play();
