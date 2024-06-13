8444595ab8cb13693eaaec9187db2ddc285256ea
nithishakumar <nithisha@NITHISHA>
2024-06-13 14:35:57 -0400

    test api

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -4,12 +4,46 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import React, { useEffect } from 'react';
 import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/camera-kit";
+import {
+  Injectable,
+  RemoteApiService,
+  RemoteApiServices,
+  remoteApiServicesFactory,
+} from '@snap/camera-kit';
 
 
 async function renderAR() {
+
+  const remoteApiService: RemoteApiService = {
+    apiSpecId: 'b032a38e-4fb9-4719-a0b5-b31d1ce63900',
+    getRequestHandler(request) {
+      if (request.endpointId !== 'facearendpoint1') return;
+      console.log(request);
+      return (reply) => {
+        reply({
+          status: 'success',
+          metadata: {},
+          body: new TextEncoder().encode(JSON.stringify('response from app!')),
+        });
+      };
+    },
+  };
+
   // Bootstrap the CameraKit Web SDK: Download WebAssembly runtime and configure SDK
-  const apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE1NjQwNjk0LCJzdWIiOiI5YmFmZmI5Ni01ZDNlLTQ2MzUtYmIwNC00ZGFhYzNiZmQ0YTh-U1RBR0lOR345ZjJkN2Q5Ni00OTdkLTQ1YjYtODcxYy1mZDg4MzcxYjRmNTYifQ.k-Rl8uGTuACnIfjZrjMi4OSC26OStxBF6wjipB_hDYI";
-  const cameraKit = await bootstrapCameraKit({ apiToken });
+  const cameraKit = await bootstrapCameraKit(
+    {
+      apiToken:
+        "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE1NjQwNjk0LCJzdWIiOiI5YmFmZmI5Ni01ZDNlLTQ2MzUtYmIwNC00ZGFhYzNiZmQ0YTh-U1RBR0lOR345ZjJkN2Q5Ni00OTdkLTQ1YjYtODcxYy1mZDg4MzcxYjRmNTYifQ.k-Rl8uGTuACnIfjZrjMi4OSC26OStxBF6wjipB_hDYI",
+    },
+    (container) =>
+      container.provides(
+        Injectable(
+          remoteApiServicesFactory.token,
+          [remoteApiServicesFactory.token] as const,
+          (existing: RemoteApiServices) => [...existing, remoteApiService]
+        )
+      )
+  );
 
   // Let CameraKit create a new canvas, then append it to the DOM
   const canvasContainer = document.getElementById("canvas-container");
@@ -17,8 +51,7 @@ async function renderAR() {
   // Create a CameraKitSession to render lenses
   const session = await cameraKit.createSession();
   if(!canvasContainer) {
-    console.log("canvasContainer was null");
-    return;
+    return
   }
   canvasContainer.appendChild(session.output.live);
 
@@ -34,11 +67,7 @@ async function renderAR() {
   // This is the test lens we made to test our API
   var tempLensId = "0e0463a1-11bc-403b-96ea-27f9dac90730";
   const lens = await cameraKit.lensRepository.loadLens(tempLensId, lensGroupID);
-  await session.applyLens(
-    lens,
-    { launchParams: { text: "Some Text that we will use with a Lens" } }
-);
-
+  await session.applyLens(lens);
   await session.play();
 }
 
