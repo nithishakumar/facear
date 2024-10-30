d2b2ea7a8d7961ffb15b61e041f5ab4e880349d5
missyfang <57602734+missyfang@users.noreply.github.com>
2024-10-30 16:11:15 -0700

    Revert "hide demo page behind auth and login"
    This reverts commit f2388a9ea2c94a4a6d76a5869197888dbfd1650b.

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,18 @@ import Home from './Home';
 import Development from './Demo';
 import Footer from './Footer';
 import About from './About';
-import Login from './Login';
-import AuthProvider from './AuthProvider';
-import ProtectedRoute from './ProtectedRoute';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-        <AuthProvider>
         <FaceARNavbar />
         <div className='p-4 mb-4'></div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route element={<ProtectedRoute />}>
-              <Route path="/demo" element={<Development />} />
-            </Route>
+          <Route path="/demo" element={<Development />} />
         </Routes>
-        </AuthProvider>
         <div className='p-4 mb-4'></div>
         <Footer />
       </Router>
